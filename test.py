a5c8236870a733e6fb2f965b6f5b29ec80fa392a
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-07-28 00:05:03 +0600

    completed

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-num1=int(input())
-num2=int(input())
+inp=input()
+numbers=inp.split() 
+num1=int(numbers[0])
+num2=int(numbers[1])
 lastdigitnum1=num1%10
 lastdigitnum2=num2%10
 sum=lastdigitnum1+lastdigitnum2
