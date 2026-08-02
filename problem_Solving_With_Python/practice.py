581e70f8cef0154b25347e342d32ecb5d793e9fb
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-08-02 23:57:20 +0600

    added

--- problem_Solving_With_Python/practice.py
+++ problem_Solving_With_Python/practice.py
@@ -4,4 +4,10 @@ min_value = float(input())
 print("Please enter the maximum value of the range:")
 max_value = float(input())
 print("Please enter the number you want to scale:")
-number = float(input()) 
+number = float(input())
+def min_max_scaling(num, min_val, max_val):
+    scaled_value = (num - min_val) / (max_val - min_val)
+    return scaled_value
+scaled_number = min_max_scaling(number, min_value, max_value)
+print(f"The scaled value of {number} in the range [{min_value}, {max_value}] is: {scaled_number}")  
+ 
