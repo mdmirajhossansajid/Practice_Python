d85d5dc6255f3a983cff643e0a785b494ad735b2
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-08-08 00:14:24 +0600

    biased unbaised check with map

--- AI_Bias_Detector.py
+++ AI_Bias_Detector.py
@@ -1,12 +1,21 @@
+# inp=input().split()
+# A=0; B=0
+# for i in range(len(inp)):
+#     if inp[i]=="A":
+#         A+=1
+#     else:
+#         B+=1
+# total=len(inp)
+# if A> total*0.7 or B> total*0.7:
+#     print("Biased Model")
+# else:
+#     print("Fair Model")
+ #using map and lamda function
 inp=input().split()
-A=0; B=0
-for i in range(len(inp)):
-    if inp[i]=="A":
-        A+=1
-    else:
-        B+=1
+a=list(map(lambda x: x=="A", inp))
+b=list(map(lambda x: x=="B", inp))
 total=len(inp)
-if A> total*0.7 or B> total*0.7:
+if sum(a)> total*0.7 or sum(b)> total*0.7:
     print("Biased Model")
 else:
     print("Fair Model")
