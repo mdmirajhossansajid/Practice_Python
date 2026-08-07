90305e7029983179b74c66619ccc7d6dd9fc511a
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-08-07 23:29:07 +0600

    AI Mood Detector problem

--- AI_Mood_Detector.py
+++ AI_Mood_Detector.py
@@ -1,3 +1,7 @@
 s=input()
 if "happy" in s or "joy" in s or "smile" in s:
     print("Happy Mood")
+elif "sad" in s or "cry"in s or "angry" in s :
+    print("Sad Mood")
+else:
+    print("Neutral Mood")
