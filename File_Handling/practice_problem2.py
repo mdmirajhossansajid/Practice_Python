ef961ab810ea6988d808101c86e74681e800afac
Miraj Hossan Sajid <mdmirajhossansajidpc@gmail.com>
2026-08-17 16:47:44 +0600

    Create practice_problem2.py

---
+++ File_Handling/practice_problem2.py
@@ -0,0 +1,13 @@
+QUESTION:::
+--------------------------------------------------------------------------------------------------------------------
+Create a text file named names.txt containing a list of names.
+Some names may have extra spaces, inconsistent capitalization (like “aLiCe” or “ bob ”), or appear more than once.
+Write a Python program to clean the data by:
+1.  Removing extra spaces.
+2.  Converting all names so that only the first letter is capitalized.
+3.  Removing duplicate names. 
+4. Sorting the names alphabetically.
+Finally, save the cleaned list into a new file named clean_names.txt.
+-----------------------------------------------------------------------------------------------------------------------
+
+
